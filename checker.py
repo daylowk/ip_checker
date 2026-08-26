941c9dc65325c6b6080de80f7198f2c21b65be79
Daylon <d3f4l7gt5@gmail.com>
2026-08-26 11:39:56 -0300

    feat: initial configs

--- checker.py
+++ checker.py
@@ -1,1 +1,20 @@
-print('Hello, World!')
+import ipaddress
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('ABUSEIPDB_API_KEY')
+
+if api_key is None:
+    raise RuntimeError('ABUSEIPDB_API_KEY is not set.')
+
+print(api_key)
+
+ip = '173.234.31.186'
+
+try:
+    ipaddress.IPv4Address(ip)
+    print(ip)
+except ipaddress.AddressValueError:
+    print('IP inválido')
