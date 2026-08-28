9ea1d7d12f020ae2dea5392e64c247f38eb49c20
Daylon <d3f4l7gt5@gmail.com>
2026-08-28 12:22:53 -0300

    feat: abuseipdb request

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import ipaddress
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,12 +10,26 @@ api_key = os.getenv('ABUSEIPDB_API_KEY')
 if api_key is None:
     raise RuntimeError('ABUSEIPDB_API_KEY is not set.')
 
-print(api_key)
-
-ip = '173.234.31.186'
+ip = '10.0.0.255'
 
 try:
     ipaddress.IPv4Address(ip)
     print(ip)
 except ipaddress.AddressValueError:
     print('IP inválido')
+
+url = 'https://api.abuseipdb.com/api/v2/check'
+
+headers = {
+    'key': api_key,
+    'Accept': 'application/json'
+}
+
+params = {
+    'ipAddress': ip
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print(response.status_code)
+print(response.json())
