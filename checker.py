cac9a8b47a8a9ca97dafa60bb39113f7d0745ffa
Daylon <d3f4l7gt5@gmail.com>
2026-09-03 08:52:57 -0300

    feat: CLI added and refatoration

--- checker.py
+++ checker.py
@@ -1,5 +1,7 @@
+import argparse
 import ipaddress
 import os
+
 import requests
 from dotenv import load_dotenv
 
@@ -10,26 +12,38 @@ api_key = os.getenv('ABUSEIPDB_API_KEY')
 if api_key is None:
     raise RuntimeError('ABUSEIPDB_API_KEY is not set.')
 
-ip = '10.0.0.255'
+def check_ip(ip):
+    url = 'https://api.abuseipdb.com/api/v2/check'
+
+    headers = {
+    'key': api_key,
+    'Accept': 'application/json'
+    }
+
+    params = {
+    'ipAddress': ip
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    return response
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'ip',
+    metavar='IP',
+    help='IP address used for the consultation.'
+)
+
+args = parser.parse_args()
+
+ip = args.ip
 
 try:
     ipaddress.IPv4Address(ip)
+    response = check_ip(ip)
     print(ip)
+    print(response.status_code)
+    print(response.json())
 except ipaddress.AddressValueError:
     print('IP inválido')
-
-url = 'https://api.abuseipdb.com/api/v2/check'
-
-headers = {
-    'key': api_key,
-    'Accept': 'application/json'
-}
-
-params = {
-    'ipAddress': ip
-}
-
-response = requests.get(url, headers=headers, params=params)
-
-print(response.status_code)
-print(response.json())
